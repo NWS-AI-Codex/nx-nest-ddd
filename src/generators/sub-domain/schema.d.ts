4d91901f113fe21ff5a3871b58ffc39aaeeffea5
Ha Tung <hatung@gmail.com>
2025-06-03 01:41:10 +0700

    add ocoda event sourcing generator

--- src/generators/sub-domain/schema.d.ts
+++ src/generators/sub-domain/schema.d.ts
@@ -10,4 +10,16 @@ export interface SubDomainGeneratorSchema {
   e2eTestRunner?: 'jest' | 'none';
   setParserOptionsProject?: boolean;
   strict?: boolean;
+  testEnvironment?: 'jsdom' | 'node';
+  target?:
+    | 'es5'
+    | 'es6'
+    | 'esnext'
+    | 'es2015'
+    | 'es2016'
+    | 'es2017'
+    | 'es2018'
+    | 'es2019'
+    | 'es2020'
+    | 'es2021';
 }
